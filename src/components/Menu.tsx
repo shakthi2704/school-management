c10ac68a05117efd8b500f775bb89f8673f6efa6
Satheesh <59468802+shakthi2704@users.noreply.github.com>
2024-12-03 14:51:33 +0530

    menu-and-nav

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -111,4 +111,34 @@ const menuItems = [
       },
     ],
   },
-];
+]
+
+import Image from "next/image"
+import Link from "next/link"
+import React from "react"
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
+          </span>
+          {i.items.map((item) => (
+            <Link
+              href={item.href}
+              key={item.label}
+              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+            >
+              <Image src={item.icon} alt={item.label} height={20} width={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Menu
